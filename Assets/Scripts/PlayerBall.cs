918aabcdcedea0b393c29d2f85def67e7d3030fa
김현승 <corn120033@gmail.com>
2022-04-16 14:49:59 +0900

    First commit

---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }sadasdasd
+
+    void FixedUpdate()
+    {
+        
+    }
+}
