243f68bb40d4792788a781e9d8218de88b8cf08c
김현승 <corn120033@gmail.com>
2022-05-11 23:17:05 +0900

    Update Capsule.cs
    충돌 인식과 충돌로 인한 이벤트들을 삭제한다

--- Assets/Scripts/Capsule.cs
+++ Assets/Scripts/Capsule.cs
@@ -5,18 +5,9 @@ using UnityEngine;
 public class Capsule : MonoBehaviour
 {
     public float rotateSpeed;
+
     void Update()
     {
         transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.name == "Player")
-        {
-            PlayerBall player = other.GetComponent<PlayerBall>();
-            player.itemCount++;
-            gameObject.SetActive(false);
-        }
     }
 }
