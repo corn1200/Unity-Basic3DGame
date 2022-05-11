79d296b44a1183eb5e52bec60d56ef9f3777e2c2
김현승 <corn120033@gmail.com>
2022-05-11 22:39:22 +0900

    Update Capsule.cs
    플레이어 유닛이 아이템에 닿았을 때 아이템을 없애고 점수를 추가한다

--- Assets/Scripts/Capsule.cs
+++ Assets/Scripts/Capsule.cs
@@ -9,4 +9,14 @@ public class Capsule : MonoBehaviour
     {
         transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            PlayerBall player = other.GetComponent<PlayerBall>();
+            player.itemCount++;
+            gameObject.SetActive(false);
+        }
+    }
 }
