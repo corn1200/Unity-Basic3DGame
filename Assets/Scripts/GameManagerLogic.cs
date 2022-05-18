a4e7c844a8a865722e492dc6a5ab0cf46549c6ad
김현승 <corn120033@gmail.com>
2022-05-18 23:41:26 +0900

    Update GameManagerLogic.cs
    스테이지 시작 시 클리어를 위한 아이템 갯수를 출력하고, 아이템을 획득할 때 마다 현재 획득 아이템을 갱신한다

--- Assets/Scripts/GameManagerLogic.cs
+++ Assets/Scripts/GameManagerLogic.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManagerLogic : MonoBehaviour
 {
     public int totalItemCount;
     public int stage;
+    public Text stageCountText;
+    public Text playerCountText;
+
+    private void Awake()
+    {
+        stageCountText.text = totalItemCount.ToString();
+    }
+
+    public void getItem(int count)
+    {
+        playerCountText.text = count.ToString();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
